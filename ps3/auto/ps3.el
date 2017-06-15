4709e80fcf69388b695fce850ca0de5c31ebf18f
Nandan Rao <nandanmarkrao@gmail.com>
2017-06-15 14:55:58 +0200

    ps3

---
+++ ps3/auto/ps3.el
@@ -0,0 +1,42 @@
+(TeX-add-style-hook
+ "ps3"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "a4paper" "12pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("biblatex" "backend=biber") ("algpseudocode" "noend")))
+   (add-to-list 'LaTeX-verbatim-environments-local "VerbatimOut")
+   (add-to-list 'LaTeX-verbatim-environments-local "SaveVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "LVerbatim*")
+   (add-to-list 'LaTeX-verbatim-environments-local "LVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "BVerbatim*")
+   (add-to-list 'LaTeX-verbatim-environments-local "BVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "Verbatim*")
+   (add-to-list 'LaTeX-verbatim-environments-local "Verbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "Verb")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "mathtools"
+    "amsfonts"
+    "amssymb"
+    "amsmath"
+    "bm"
+    "commath"
+    "multicol"
+    "algorithmicx"
+    "tkz-graph"
+    "algorithm"
+    "fancyhdr"
+    "pgfplots"
+    "fancyvrb"
+    "biblatex"
+    "algpseudocode")
+   (LaTeX-add-bibliographies))
+ :latex)
+
