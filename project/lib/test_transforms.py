e7f9771063ed1c33d6f4a0f88ef860611e5832af
Nandan Rao <nandanmarkrao@gmail.com>
2017-06-21 19:42:00 +0200

    matrix-form refactor of equilibrium algo

--- project/lib/test_transforms.py
+++ project/lib/test_transforms.py
@@ -1,41 +1,102 @@
 from transforms import *
 import networkx as nx
+import random
 
-def basic_G():
-    G = nx.DiGraph()
-    G.add_nodes_from([1,2,3])
-    G.add_edges_from([(1,2), (1,3)], flow = 1)
-    G.add_edges_from([(2,3)], flow = 0)
-    return G
-
-def test_edge_in_path():
-    G = basic_G()
-    assert edge_in_path((1,2), G.edges(data=True)) == True
-    assert edge_in_path((1,2), G.edges()) == True
-    assert edge_in_path((1,2,{'flow': 1}),  G.edges(data=True)) == True
-    assert edge_in_path((1,2,{'flow': 1}), G.edges()) == True
-
-def test_potential_network_adds_edge_weights():
-    G = basic_G()
-    p = [(1,2), (1,3)]
-    new_G = potential_network(G, p)
-    inverted = list(new_G.edges(data = True))
-    assert map(lambda t: t[2]['flow'], inverted) == [1,1,1]
-
-def test_compute_distance_applies_function():
-    G = basic_G()
+def test_get_distance_applies_function():
+    paths = [[(1,2), (2,3), (3,6)], [(2,3), (3,6), (6,9)]]
+    state = np.stack([path_to_mat(p, 10) for p in paths])
     fn = lambda f: 1/f if f > 0 else float('inf')
-    with_weights = list(compute_distance(G, fn).edges(data=True))
-    assert map(lambda t: t[2]['weight'], with_weights) == [1,1,float('inf')]
+    d = get_distance(state, fn)
+    assert d[2,3] == .5
+    assert d[1,2] == 1
+    assert d[0,0] == float('inf')
 
-def test_update_graph_with_path():
-    G = nx.DiGraph()
-    G.add_nodes_from([1,2,3,4])
-    G.add_edges_from([(1,2), (1,3), (3,4), (4,1)], flow = 1)
-    G.add_edges_from([(2,3)], flow = 0)
-    old_path = [(1,2), (1,3)]
-    new_path = [(1,2), (2,3), (3,4)]
-    ug = update_graph_with_path(G, old_path, new_path)
-    edges = list(ug.edges(data=True))
-    expected = [(1,2, {'flow': 1}), (1,3, {'flow': 0}), (3,4, {'flow': 2}), (4,1,{'flow': 1}), (2,3, {'flow': 1})]
-    assert sorted(edges, key = lambda x: x[0]) == sorted(expected, key = lambda x: x[0])
+def test_get_potential_state():
+    paths = [[(1,2), (2,3), (3,4)], [(2,3), (3,4)]]
+    state = np.stack([path_to_mat(p, 5) for p in paths])
+
+    # make sure it works with nan's
+    state[1,1] = np.nan
+    new_state = get_potential_state(state, 1)
+
+    # check that there are no zeros on any edge
+    assert np.any(np.sum(new_state, axis=0) == 0) == False
+
+def test_update_path():
+    paths = [[(1,2), (2,3), (3,4)], [(2,3), (3,4)]]
+    state = np.stack([path_to_mat(p, 5) for p in paths])
+    state[0,0,0] = np.nan
+    new_state = update_path(state, [(2,3), (3,1)], 1)
+
+    assert np.isnan(new_state[0,0,0])
+    assert new_state[1,2,3] == 1
+    assert new_state[1,3,1] == 1
+    assert new_state[1,3,4] == 0
+
+def test_apply_nans():
+    nan_mat = np.array([[np.nan, 1.], [2.,3.]])
+    mat = np.array([[3.,4.], [5.,6.]])
+    applied = apply_nans(mat, nan_mat)
+    assert np.isnan(applied[0,0])
+    assert applied[1,1] == 6
+
+# def test_get_path():
+#     paths = [[(1,2), (2,3), (3,4)], [(2,3), (3,4)]]
+#     state = np.stack([path_to_mat(p, 5) for p in paths])
+#     assert get_path(state, 0) == [(1,2), (2,3), (3,4)]
+
+def test_start_end_a():
+    s = [(9,7), (7,2), (2,8), (8,1)]
+    m = path_to_mat(s, 10)
+    m[0,0] = np.nan
+    assert start_end(m) == (9,1)
+
+    # Make sure it's not fluke
+    s = [(1,7), (7,2), (2,8), (8,9)]
+    m = path_to_mat(s, 10)
+    m[0,0] = np.nan
+    assert start_end(m) == (1,9)
+
+def test_get_start_end():
+    paths = [[(1,2), (2,3), (3,4)], [(2,3), (3,4)]]
+    state = np.stack([path_to_mat(p, 5) for p in paths])
+    assert get_start_end(state, 1) == ((2,3), (3,4))
+
+def test_start_end_a():
+    s = [(9,7), (7,2), (2,8), (8,1)]
+    m = path_to_mat(s, 10)
+    m[0,0] = np.nan
+    assert start_end(m) == (9,1)
+
+    # Make sure it's not fluke
+    s = [(1,7), (7,2), (2,8), (8,9)]
+    m = path_to_mat(s, 10)
+    m[0,0] = np.nan
+    assert start_end(m) == (1,9)
+
+def test_arrange_path():
+    s = [(9,7), (7,2), (2,8), (8,1)]
+    m = path_to_mat(s, 10)
+    m[0,0] = np.nan
+    assert arrange_path(m, [9]) == [(9,7), (7,2), (2,8), (8,1)]
+
+def test_node_to_edge_list():
+    assert node_to_edge_list([1,2,3,4,5]) == [(1,2), (2,3), (3,4), (4,5)]
+
+def test_get_next_path():
+    paths = [[(1,2), (2,3), (3,4)], [(2,3), (3,4)]]
+    state = np.stack([path_to_mat(p, 5) for p in paths])
+    fn = lambda f: 1/f if f > 0 else float('inf')
+    new_state = get_next_path(state, 0, fn)
+
+    # TODO: properly test and make a real assertion...
+    assert np.all(new_state == state) == False
+
+def test_find_equilibrium():
+    paths = [[(1,2), (2,3), (3,4)], [(2,3), (3,4)], [(2,3), (3,4)]]
+    state = np.stack([path_to_mat(p, 5) for p in paths])
+    fn = lambda f: 1/f if f > 0 else float('inf')
+    new_state = find_equilibrium(state, fn)
+
+    # TODO: properly test and make a real assertion...
+    assert np.all(new_state == state) == True
--- project/lib/transforms.py
+++ project/lib/transforms.py
@@ -1,51 +1,102 @@
 import networkx as nx
-from toolz import assoc
+import numpy as np
 
-def simple_path(path):
-    return ((e[0], e[1]) for e in path)
+def get_path(m):
+    """ returns matrix path in list of tuples format"""
+    s,f = start_end(m)
+    return arrange_path(m, [s])
 
-def edge_in_path(e, p):
-    return (e[0], e[1]) in simple_path(p)
+def node_to_edge_list(l):
+    """ Takes a list of nodes and turns into a list of edges """
+    fn = lambda a,b: a + [(a[-1][1], b)]
+    return reduce(fn, l[2:], [(l[0], l[1])])
 
-def inc_attr(e, attr, amt):
-    return e[0], e[1], { attr: e[2][attr]+amt }
+def get_end(m,i):
+    return np.nonzero(m[i,:] == 1)[0][0]
 
-def inc_graph(G, path, attr, amt):
-    edges = G.edges(data=True)
-    G.add_edges_from([inc_attr(e, attr, amt)
-                      for e in edges if edge_in_path(e, path)])
-    return G
+def arrange_path(m, ordered):
+    """ assumes no self loops! """
+    while True:
+        e = ordered[-1]
+        try:
+            # get the column of the non-zero element of this row
+            # this will throw if there is none, showing we're done.
+            n = get_end(m,e)
+            ordered.append(n)
+        except IndexError:
+            return node_to_edge_list(ordered)
 
-def potential_network(G, path):
-    """ Returns the network as seen by an agent with path p. """
-    edges = list(G.edges()) # make list because we iterate it twice
-    to_explore = [e for e in edges if e not in path]
-    return inc_graph(G, to_explore, 'flow', 1)
+def sorted_path(m):
+    """ Sorted path in order of travel """
+    s,f = start_end(m)
+    return path
 
-def calc_weight(fn, e):
-    return e[0], e[1], assoc(e[2], 'weight', fn(e[2]['flow']))
+def start_end(m):
+    """ start and end node of path matrix """
+    m = np.nan_to_num(m)
+    a,b = m.sum(axis=1), m.sum(axis=0)
+    summed = a + b
+    s,f =  np.nonzero(summed == 1)[0]
+    if np.any(s  == np.nonzero(b == 1)):
+        s,f = f,s
+    return s,f
 
-def compute_distance(G, fn):
-    """ Calculates edge weights based on flow and flow function fn """
-    edges = G.edges(data=True)
-    G.add_edges_from([calc_weight(fn, e) for e in edges])
-    return G
+def get_start_end(state, i):
+    """ Returns start and end node of an agent """
+    m = state[i,:,:]
+    p = get_path(m)
+    return (p[0], p[-1])
 
-def update_graph_with_path(G, old_path, new_path):
-    G = inc_graph(G, old_path, 'flow', -1)
-    G = inc_graph(G, new_path, 'flow', 1)
-    return G
+def apply_nans(mat, nan_mat):
+    """ MUTATES MAT -- makes that shit nan."""
+    mat[np.isnan(nan_mat)] = np.nan
+    return mat
 
-def get_path(A):
-    """ Returns current path for given agent """
-    return A
+def update_path(state, path, i):
+    """ Updates the state with a new path for a given agent """
+    N = state.shape[1]
+    update = path_to_mat(path, N)
+    s = np.copy(state)
+    s[i,:,:] = update
+    return apply_nans(s, state)
 
-def find_new_path(G, A, fn):
-    """ Returns new graph and agent with new best path."""
-    path = get_path(A)
-    fov = potential_network(G, path)
-    compute_distance(G, fn)
-    shortest_path = nx.dijkstra_path(G, path)
-    new_G = update_graph_with_path(G, opath, shortest_path)
-    new_A = update_agent(A, new_path)
-    return new_G, new_A
+def get_potential_state(state, i):
+    s = np.copy(state)
+    s[i,:,:] = 1
+    return s
+
+def path_to_mat(path, N):
+    a = np.zeros((N, N))
+    for x,y in path:
+        a[x,y] = 1
+    return a
+
+def get_distance(state, fn):
+    flow = np.sum(state, axis=0)
+    vfn = np.vectorize(fn, otypes=[np.float])
+    distance = vfn(flow)
+    return distance
+
+def make_graph(dist):
+    return nx.DiGraph(dist)
+
+def get_shortest_path(dist, start, end):
+    G = make_graph(dist)
+    p = nx.dijkstra_path(G, start[0], end[1])
+    return node_to_edge_list(p)
+
+def get_next_path(state, i, fn):
+    pot = get_potential_state(state, i)
+    dist = get_distance(pot, fn)
+    s,e = get_start_end(state, i)
+    path = get_shortest_path(dist, s, e)
+    new_state = update_path(state, path, i)
+    return new_state
+
+def find_equilibrium(state, fn):
+    new_state = np.copy(state)
+    for i in range(state.shape[0]):
+        new_state = get_next_path(state, i, fn)
+    if np.all(new_state == state):
+        return state
+    return find_equilibrium(new_state, fn)
